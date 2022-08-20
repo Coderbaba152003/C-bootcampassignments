e8bc5f4f1deed2fe5d4bd02eb7cf977965476ac9
Coderbaba152003 <107532260+Coderbaba152003@users.noreply.github.com>
2022-08-20 21:16:45 +0530

    Add files via upload

---
+++ Assignment23.cpp
@@ -0,0 +1,102 @@
+// All questions are solved in this file.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    // solution of question no 1.
+    // cout<<"Hello MySirG";
+
+
+
+
+    // solution of question no 2.
+    // cout<<"Hello"<<endl<<"MySirG";
+
+
+
+
+    //solution of question no 3.
+    // float num1,num2;
+    // cout<<"Enter two numbers:"<<endl;
+    // cin>>num1>>num2;
+    // cout<<"Sum is "<<num1+num2;
+
+
+
+
+    // solution of question no 4.
+    // float radius;
+    // cout<<"Enter radius of circle:"<<endl;
+    // cin>>radius;
+    // cout<<"Radius is => "<<(3.14*radius*radius)<<" sq units";
+
+
+
+
+    // solution of question no 5.
+    // float length,width,height;
+    // cout<<"Enter length,width and height of cuboid:"<<endl;
+    // cin>>length>>width>>height;
+    // cout<<"Volume of the cuboid is => "<<(length*width*height);
+
+
+
+
+    // solution of question no 6.
+    // float num1,num2,num3;
+    // cout<<"Enter three numbers:"<<endl;
+    // cin>>num1>>num2>>num3;
+    // cout<<"Average of the numbers is => "<<((num1+num2+num3)/3);
+
+
+
+
+    // solution of question no 7.
+    // float x;
+    // cout<<"Enter a number:"<<endl;
+    // cin>>x;
+    // cout<<"Square of the number is => "<<x*x;   
+
+
+
+
+    // solution of question no 8.
+    // int var1, var2;
+    // cout<<"Enter two int variables:"<<endl;
+    // cin>>var1>>var2;
+    // var1 = var1 + var2;
+    // var2 = var1 - var2;
+    // var1 = var1 - var2;
+    // cout<<"After swapping =>"<<endl;
+    // cout<<"Num1 => "<< var1 << endl <<"Num2 => "<< var2;
+
+
+
+
+    // solution of question no 9.
+    // int num1,num2;
+    // cout<<"Enter two numbers:"<<endl;
+    // cin>>num1>>num2;
+    // if(num1 > num2)
+    // cout<<"Maximum is "<<num1;
+    // else
+    // cout<<"Maximum is "<<num2;
+
+
+
+
+    // solution of question no 10.
+    // int arr[10],sum=0;
+    // cout<<"Enter the 10 numbers into array:"<<endl;
+    // for(int i=0; i<=9; i++)
+    // {
+    //     cin>>arr[i];
+    //     sum = sum + arr[i];
+    // }
+    // cout<<"Sum of the numbers is => "<<sum;
+
+
+    
+        
+}
